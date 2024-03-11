5007e302d3e2001d735dce31674b2b7b7594388e
gdtknight <gdtknight@naver.com>
2024-03-11 21:46:13 +0900

    initialize spring boot project - 2.7.14

---
+++ src/test/java/kr/yongdev/backend/BackendApplicationTest.java
@@ -0,0 +1,10 @@
+package kr.yongdev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTest {
+  @Test
+  void contextLoads() {}
+}
